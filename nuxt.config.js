b8b63d174f823a9b462bb4657d42f55cb43643db
Alban Mouton <alban.mouton@gmail.com>
2020-11-09 12:36:58 +0100

    feat: dark mode

--- nuxt.config.js
+++ nuxt.config.js
@@ -35,16 +35,27 @@ module.exports = {
   buildModules: ['@nuxtjs/vuetify'],
   vuetify: {
     theme: {
+      dark: false,
       themes: {
         light: {
-          primary: '#1E88E5', // colors.blue.darken1
-          // primary: colors.blue.lighten1, // code near our logo 'dark' blue
-          accent: '#F57C00', // colors.orange.darken2
-          warning: '#F57C00' // colors.orange.darken2
+          primary: '#1E88E5', // blue.darken1
+          secondary: '#42A5F5', // blue.lighten1,
+          accent: '#FF9800', // orange.base
+          error: 'FF5252', // red.accent2
+          info: '#2196F3', // blue.base
+          success: '#4CAF50', // green.base
+          warning: '#E91E63', // pink.base
+          admin: '#E53935' // red.darken1
         },
         dark: {
-          primary: '#42A5F5', // colors.blue.lighten1,
-          accent: '#FF9800' // colors.orange.base
+          primary: '#2196F3', // blue.base
+          secondary: '#42A5F5', // blue.lighten1,
+          accent: '#FF9800', // orange.base
+          error: 'FF5252', // red.accent2
+          info: '#2196F3', // blue.base
+          success: '#00E676', // green.accent3
+          warning: '#E91E63', // pink.base
+          admin: '#E53935' // red.darken1
         }
       }
     }
--- public/plugins/session.js
+++ public/plugins/session.js
@@ -1,4 +1,6 @@
-export default ({ store, app, env }) => {
+export default ({ store, app, env, route, $vuetify }) => {
   store.dispatch('session/init', { cookies: app.$cookies, baseUrl: env.publicUrl + '/api/v1/session' })
   store.dispatch('session/loop', app.$cookies)
+  if (app.$cookies.get('theme_dark') !== undefined) $vuetify.theme.dark = app.$cookies.get('theme_dark')
+  if (route.query.dark) $vuetify.theme.dark = route.query.dark === 'true'
 }
