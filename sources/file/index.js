fa494a2c883808da53b0dc706741ef3d988f33e7
Nicolas Bonnel <nicolas.bonnel@gmail.com>
2020-12-02 17:09:21 +0100

    fix: remote file name decoding

--- sources/file/index.js
+++ sources/file/index.js
@@ -8,6 +8,6 @@ exports.run = async function(config) {
   const res = await axios.get(config.url, { responseType: 'stream' })
   const tmpFile = await file()
   await pump(res.data, fs.createWriteStream(tmpFile.path))
-  const fileName = res.headers['content-disposition'] ? res.headers['content-disposition'].match(/filename="(.*)"/)[1] : path.parse(config.url).base
+  const fileName = res.headers['content-disposition'] ? res.headers['content-disposition'].match(/filename="(.*)"/)[1] : decodeURIComponent(path.parse(config.url).base)
   return { fileName, tmpFile }
 }
