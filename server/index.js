5d9c7581c438f195aae68d15cfe17efd80431c0f
Alban Mouton <alban.mouton@gmail.com>
2021-01-29 22:13:04 +0100

    feat: work on plugins registry from npm

--- server/index.js
+++ server/index.js
@@ -1,98 +1,21 @@
+
 const config = require('config')
-const express = require('express')
-const http = require('http')
-const { URL } = require('url')
-const event2promise = require('event-to-promise')
-const bodyParser = require('body-parser')
-const cookieParser = require('cookie-parser')
-const proxy = require('http-proxy-middleware')
-const api = require('./api')
-const nuxt = require('./nuxt')
-const dbUtils = require('./utils/db')
-const scheduler = require('./utils/scheduler')
-const session = require('@koumoul/sd-express')({
-  publicUrl: config.publicUrl,
-  directoryUrl: config.directoryUrl,
-  cookieDomain: config.sessionDomain
-})
-const debug = require('debug')('main')
+const app = require('./app')
 
-const publicHost = new URL(config.publicUrl).host
-debug('Public host', publicHost)
-
-// Second express application for proxying requests based on host
-const app = express()
-
-// re-expose remote data-fair using local api-key
-const dataFairUrl = new URL(config.dataFairUrl)
-const dataFairIsLocal = new URL(config.publicUrl).origin === dataFairUrl.origin
-if (!dataFairIsLocal) {
-  app.use('/data-fair-proxy', session.auth, proxy({
-    target: dataFairUrl.origin,
-    pathRewrite: { '^/data-fair-proxy': dataFairUrl.pathname },
-    secure: false,
-    logLevel: 'debug',
-    changeOrigin: true,
-    onProxyReq (proxyReq, req, res) {
-      if (!req.user || !req.user.adminMode) return res.status(403).send('Super admin only')
-      proxyReq.setHeader('cookie', '')
-      proxyReq.setHeader('x-apiKey', config.dataFairAPIKey)
-    }
-  }))
-}
-
-if (process.env.NODE_ENV === 'development') {
-  // Create a mono-domain environment with other services in dev
-  app.use('/simple-directory', proxy({ target: 'http://localhost:5700', pathRewrite: { '^/simple-directory': '' } }))
-  app.use('/data-fair', proxy({ target: 'http://localhost:8080', pathRewrite: { '^/data-fair': '' }, ws: true }))
-}
-
-app.use(cookieParser())
-app.use(bodyParser.json())
-app.use(bodyParser.text())
-
-app.use('/api/v1/session', session.router)
-app.use('/api/v1', api)
-
-let httpServer
-async function main() {
-  const nuxtMiddleware = await nuxt()
-  app.use(session.loginCallback)
-  app.use(session.decode)
-  app.use(nuxtMiddleware)
-  const { client, db } = await dbUtils.init()
-  app.set('db', db)
-  await require('../upgrade')(db)
-  app.set('client', client)
-  scheduler.init(db)
-  app.use((err, req, res, next) => {
-    console.error('Error in HTTP request', err.response ? err.response.data : err)
-    res.status(err.status || 500).send(err.message)
-  })
-
-  httpServer = http.createServer(app).listen(config.port)
-  await event2promise(httpServer, 'listening')
-  debug('HTTP server is listening', config.port)
-}
-
-main().then(() => {
-  console.log('Running on ' + config.publicUrl)
+app.run().then(app => {
+  console.log('Web socket and HTTP server listening on http://localhost:%s', config.port)
 }, err => {
-  console.error(err)
+  console.error('Failure in service process', err)
   process.exit(-1)
 })
 
-process.on('SIGTERM', async function onSigterm () {
+process.on('SIGTERM', function onSigterm () {
   console.info('Received SIGTERM signal, shutdown gracefully...')
-  try {
-    if (httpServer) {
-      httpServer.close()
-      await event2promise(httpServer, 'close')
-    }
+  app.stop().then(() => {
     console.log('shutting down now')
     process.exit()
-  } catch (err) {
+  }, err => {
     console.error('Failure while stopping service', err)
     process.exit(-1)
-  }
+  })
 })
---
+++ test/setup-teardown.js
@@ -0,0 +1,74 @@
+const config = require('config')
+const fs = require('fs-extra')
+const nock = require('nock')
+const axios = require('axios')
+const app = require('../server/app')
+const axiosAuth = require('@koumoul/sd-express').axiosAuth
+
+before('global mocks', () => {
+  // fake remote service
+  nock('http://registry.npmjs.com')
+    .persist()
+    .get('/-/v1/search?text=%22data-fair-processings-plugin%22%20hello%20world&size=250').reply(200, require('./resources/npm-response'))
+})
+
+before('init globals', async () => {
+  const { db, client } = await require('../server/utils/db.js').connect()
+  global.db = db
+  global.mongoClient = client
+
+  global.ax = {}
+  global.ax.builder = async (email, org, opts = {}) => {
+    opts.baseURL = config.publicUrl
+
+    let ax
+    if (email) ax = await axiosAuth(email, org, opts)
+    else ax = axios.create(opts)
+
+    // customize axios errors for shorter stack traces when a request fails in a test
+    ax.interceptors.response.use(response => response, error => {
+      if (!error.response) return Promise.reject(error)
+      delete error.response.request
+      return Promise.reject(error.response)
+    })
+    return ax
+  }
+  await Promise.all([
+    global.ax.builder().then(ax => { global.ax.anonymous = ax }),
+    global.ax.builder('dmeadus0@answers.com:passwd').then(ax => { global.ax.dmeadus = ax }),
+    global.ax.builder('dmeadus0@answers.com:passwd', 'KWqAGZ4mG').then(ax => { global.ax.dmeadusOrg = ax }),
+    global.ax.builder('cdurning2@desdev.cn:passwd').then(ax => { global.ax.cdurning2 = ax })
+  ])
+})
+
+before('scratch all', async() => {
+  await global.db.dropDatabase()
+  await fs.remove('./data/test')
+})
+
+before('start app', async function () {
+  try {
+    global.app = await app.run()
+  } catch (err) {
+    console.error('Failed to run the application', err)
+    throw err
+  }
+})
+
+beforeEach('scratch data', async () => {
+  await Promise.all([
+    global.db.collection('processings').deleteMany({}),
+    fs.emptyDir('./data/test')
+  ])
+})
+
+after('stop app', async () => {
+  await Promise.race([
+    new Promise(resolve => setTimeout(resolve, 5000)),
+    app.stop()
+  ])
+})
+
+after('cleanup globals', async () => {
+  await global.mongoClient.close()
+})
