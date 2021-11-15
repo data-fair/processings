dead1da249e59803d126cfebffd0d68c568018ec
Alban Mouton <alban.mouton@gmail.com>
2021-11-15 17:14:46 +0100

    fix: prevent using messages from children iframes for navigation

--- public/plugins/breadcrumbs.js
+++ public/plugins/breadcrumbs.js
@@ -1,8 +1,8 @@
 export default async ({ route, redirect }) => {
   // dynamic navigation from the iframe parent
   window.addEventListener('message', (e) => {
-    // message does not come from parent window but from ourself
-    if (e.source === window) return
+    // message does not come from parent window but from ourself or a child iframe
+    if (e.source === window || e.source !== window.parent) return
 
     if (e.data && typeof e.data === 'object' && 'to' in e.data && e.data.to !== route.query.to) {
       window.$nuxt.$router.replace(e.data.to)
