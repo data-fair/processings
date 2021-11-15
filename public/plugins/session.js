5d1514e49adc6afcc4ca1b9b2bba2b2a31c5a128
Alban Mouton <alban.mouton@gmail.com>
2021-11-15 15:44:16 +0100

    fix: also adapt dataFairUrl to multi-domain

--- public/plugins/session.js
+++ public/plugins/session.js
@@ -1,11 +1,14 @@
 export default ({ store, app, env, route, $vuetify }) => {
   let publicUrl = window.location.origin + env.basePath
   if (publicUrl.endsWith('/')) publicUrl = publicUrl.substr(0, publicUrl.length - 1)
+  const dataFairUrl = new URL(env.dataFairUrl)
+  dataFairUrl.host = new URL(publicUrl).host
   store.commit('setAny', {
     env: {
       ...env,
       // reconstruct this env var that we used to have but lost when implementing multi-domain exposition
       publicUrl,
+      dataFairUrl: dataFairUrl.href,
     },
   })
   store.dispatch('session/init', {
