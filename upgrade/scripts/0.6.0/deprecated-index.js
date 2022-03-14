9d57927bb74319ff44ee917e4395189239efab90
Alban Mouton <alban.mouton@gmail.com>
2022-03-14 15:46:58 +0100

    linter: add space

--- upgrade/scripts/0.6.0/deprecated-index.js
+++ upgrade/scripts/0.6.0/deprecated-index.js
@@ -3,7 +3,7 @@ exports.description = 'Remove deprecated mongodb index'
 exports.exec = async (db, debug) => {
   try {
     await db.collection('processings').dropIndex('id_1')
-  } catch(err) {
+  } catch (err) {
     // nothing to do
   }
 }
