199b73875bab4e6ee7f929c1747eb7b897851e03
Alban Mouton <alban.mouton@gmail.com>
2023-01-05 15:04:23 +0100

    chore: better responsivity in processings page

--- public/pages/processings/index.vue
+++ public/pages/processings/index.vue
@@ -25,91 +25,34 @@
         </v-container>
       </v-col>
       <layout-navigation-right v-if="$vuetify.breakpoint.lgAndUp">
-        <v-list
-          dense
-          class="list-actions"
+        <processings-actions :installed-plugins="installedPlugins" />
+        <v-card
+          v-if="user.adminMode"
+          color="admin"
+          dark
+          flat
+          class="mt-2"
         >
-          <v-menu
-            v-if="canAdmin"
-            v-model="showCreateMenu"
-            :close-on-content-click="false"
-            min-width="500px"
-            max-width="500px"
-          >
-            <template #activator="{ on, attrs }">
-              <v-list-item
-                v-bind="attrs"
-                v-on="on"
-              >
-                <v-list-item-icon>
-                  <v-icon color="primary">
-                    mdi-plus-circle
-                  </v-icon>
-                </v-list-item-icon>
-                <v-list-item-title>Créer un nouveau traitement</v-list-item-title>
-              </v-list-item>
-            </template>
-            <v-card v-if="newProcessing">
-              <v-card-title primary-title>
-                <h3 class="text-h5 mb-0">
-                  Créer un nouveau traitement
-                </h3>
-              </v-card-title>
-              <v-card-text>
-                <v-form>
-                  <v-text-field
-                    v-model="newProcessing.title"
-                    name="title"
-                    label="Titre"
-                  />
-                  <v-select
-                    v-model="newProcessing.plugin"
-                    label="Plugin"
-                    :loading="!installedPlugins.results"
-                    :items="installedPlugins.results"
-                    :item-text="item => item.name + ' - ' + item.version"
-                    item-value="id"
-                  />
-                </v-form>
-              </v-card-text>
-              <v-card-actions>
-                <v-spacer />
-                <v-btn
-                  text
-                  @click="showCreateMenu = false"
-                >
-                  Annuler
-                </v-btn>
-                <v-btn
-                  :disabled="!newProcessing.title || !newProcessing.plugin"
-                  color="primary"
-                  @click="createProcessing(newProcessing); showCreateMenu = false"
-                >
-                  Enregistrer
-                </v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-menu>
-          <v-card
-            v-if="user.adminMode"
-            color="admin"
-            dark
-            flat
-            class="mt-2"
-          >
-            <v-card-text class="pa-1">
-              <v-switch
-                v-model="showAll"
-                label="voir tous les traitements"
-                hide-details
-                dense
-                class="mt-0"
-                @change="refresh"
-              />
-            </v-card-text>
-          </v-card>
-        </v-list>
+          <v-card-text class="pa-1">
+            <v-switch
+              v-model="showAll"
+              label="voir tous les traitements"
+              hide-details
+              dense
+              class="mt-0"
+              @change="refresh"
+            />
+          </v-card-text>
+        </v-card>
       </layout-navigation-right>
+      <layout-actions-button
+        v-else
+        class="pt-2"
+      >
+        <template #actions>
+          <processings-actions :installed-plugins="installedPlugins" />
+        </template>
+      </layout-actions-button>
     </v-row>
   </v-container>
 </template>
@@ -125,14 +68,11 @@ export default {
   data: () => ({
     processings: null,
     installedPlugins: {},
-    showCreateMenu: false,
-    newProcessing: {},
     showAll: false
   }),
   computed: {
     ...mapState('session', ['user']),
-    ...mapGetters('session', ['activeAccount']),
-    ...mapGetters(['canAdmin'])
+    ...mapGetters('session', ['activeAccount'])
   },
   watch: {},
   created () {
@@ -162,18 +102,6 @@ export default {
         })
       } catch (error) {
         eventBus.$emit('notification', { error, msg: 'Erreur pendant la récupération de la liste des traitements' })
-      }
-    },
-    async createProcessing (processing) {
-      const newProcessing = await this.$axios.$post('api/v1/processings', processing)
-      this.$router.push(`/processings/${newProcessing._id}`)
-    },
-    async run (processing) {
-      try {
-        await this.$axios.$post('api/v1/processings/' + processing.id + '/_run')
-        this.refresh()
-      } catch (error) {
-        eventBus.$emit('notification', { error, msg: 'Erreur pendant le changement de statut du traitement' })
       }
     },
     format
