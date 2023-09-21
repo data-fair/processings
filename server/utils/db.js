570a5a1c75e0ed33e53444adc8cc894b43fcd387
Alban Mouton <alban.mouton@gmail.com>
2023-09-21 14:32:27 +0200

    fix: broken index main on runs collection

--- server/utils/db.js
+++ server/utils/db.js
@@ -2,14 +2,18 @@
 
 const config = require('config')
 const { MongoClient } = require('mongodb')
-const prometheus = require('./prometheus')
 
 exports.ensureIndex = async (db, collection, key, options) => {
   try {
     await db.collection(collection).createIndex(key, options || {})
-  } catch (error) {
-    prometheus.internalError.inc({ errorCode: 'mongo-index' })
-    console.error('(mongo-index) Init mongodb index creation failure for', collection, key, error)
+  } catch (err) {
+    if ((err.code !== 85 && err.code !== 86) || !options.name) throw err
+
+    // if the error is a conflict on keys or params of the index we automatically
+    // delete then recreate the index
+    console.log(`Drop then recreate index ${collection}/${options.name}`)
+    await db.collection(collection).dropIndex(options.name)
+    await db.collection(collection).createIndex(key, options)
   }
 }
 
