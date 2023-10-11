a080f00de88cfc9d15297353f34be4c1163933e0
Alban Mouton <alban.mouton@gmail.com>
2023-10-11 16:22:05 +0200

    fix: missing part of upgrade script

--- upgrade/scripts/1.21.1/remove-admin-permissions.js
+++ upgrade/scripts/1.21.1/remove-admin-permissions.js
@@ -1,10 +1,21 @@
 exports.description = 'Processings cannot have admin permissions any longer'
 
 exports.exec = async (db, debug) => {
-  const cursor = db.collection('processings').find({ 'permissions.profile': 'admin' })
-  for await (const processing of cursor) {
+  for await (const processing of db.collection('processings').find({ 'permissions.profile': 'admin' })) {
     const permissions = processing.permissions.filter(p => p.profile !== 'admin')
-    debug('clean permissions of processing', processing)
+    debug('clean permissions of processing', processing._id, processing.title, processing.permissions)
     await db.collection('processings').updateOne({ _id: processing._id }, { $set: { permissions } })
+  }
+
+  for await (const processing of db.collection('processings').find({ 'lastRun.permissions.profile': 'admin' })) {
+    const permissions = processing.lastRun.permissions.filter(p => p.profile !== 'admin')
+    debug('clean permissions of processing.lastRun', processing._id, processing.title, processing.lastRun)
+    await db.collection('processings').updateOne({ _id: processing._id }, { $set: { 'lastRun.permissions': permissions } })
+  }
+
+  for await (const run of db.collection('runs').find({ 'permissions.profile': 'admin' })) {
+    const permissions = run.permissions.filter(p => p.profile !== 'admin')
+    debug('clean permissions of processing', run._id, run.permissions)
+    await db.collection('runs').updateOne({ _id: run._id }, { $set: { permissions } })
   }
 }
