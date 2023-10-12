80703c5f7e0377184dde28ba229896ba616e8048
Alban Mouton <alban.mouton@gmail.com>
2023-10-12 09:48:00 +0200

    fix: broken admin permission

--- server/utils/permissions.js
+++ server/utils/permissions.js
@@ -50,12 +50,13 @@ const matchPermissionTarget = (target, user) => {
 }
 
 exports.getUserResourceProfile = (processing, user) => {
+  // this line is first, a manual permission cannot demote an admin
+  if (exports.getOwnerRole(processing.owner, user) === 'admin') return 'admin'
   for (const profile of ['read', 'exec']) {
     if (processing.permissions && processing.permissions.find(p => p.profile === profile && matchPermissionTarget(p.target, user))) {
       return profile
     }
   }
-  if (exports.getOwnerRole(processing.owner, user) === 'admin') return 'admin'
   if (exports.getOwnerRole(processing.owner, user) === 'contrib') return 'read'
   return null
 }
