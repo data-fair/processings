a154d0d12e1768312f91e7b7fc60a62e735b3a09
Alban Mouton <alban.mouton@gmail.com>
2024-02-13 20:07:42 +0100

    fix: unreliable test

--- test/plugins-registry.js
+++ test/plugins-registry.js
@@ -2,12 +2,12 @@ const assert = require('assert').strict
 
 describe('Plugins registry', () => {
   it('should search for plugins on npmjs', async () => {
-    const res = await global.ax.anonymous.get('/api/v1/plugins-registry', { params: { q: 'hello world' } })
+    const res = await global.ax.anonymous.get('/api/v1/plugins-registry', { params: { q: '@data-fair hello world' } })
     assert.equal(res.data.count, 2)
     assert.equal(res.data.results.length, 2)
-    assert.equal(res.data.results[0].name, '@koumoul/data-fair-processings-hello-world')
+    assert.equal(res.data.results[0].name, '@data-fair/processing-hello-world')
     assert.equal(res.data.results[0].distTag, 'latest')
-    assert.equal(res.data.results[1].name, '@koumoul/data-fair-processings-hello-world')
+    assert.equal(res.data.results[1].name, '@data-fair/processing-hello-world')
     assert.equal(res.data.results[1].distTag, 'test')
   })
 })
