91ca5c4d70b5c118d2f6fb4a930000d0ab095bcb
BatLeDev <baptiste.guerny@gmail.com>
2024-02-28 09:33:48 +0100

    fix: dependencies

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,6 +4,7 @@ module.exports = {
     node: true
   },
   extends: [
-    'standard'
+    'standard',
+    'typescript'
   ]
 }
--- worker/src/task/index.js
+++ worker/src/task/index.js
@@ -4,11 +4,9 @@ import { initPublisher } from '../../../shared/ws.mjs'
 import config from 'config'
 import { run } from './task.js'
 
-(async () => {
-  await mongo.connect(config.mongoUrl, { readPreference: 'primary' })
-  const mailTransport = nodemailer.createTransport(config.mails.transport)
-  const db = mongo.db
-  const wsPublish = await initPublisher(db)
-  const err = await run({ db, mailTransport, wsPublish })
-  if (err) process.exit(-1)
-})()
+await mongo.connect(config.mongoUrl, { readPreference: 'primary' })
+const mailTransport = nodemailer.createTransport(config.mails.transport)
+const db = mongo.db
+const wsPublish = await initPublisher(db)
+const err = await run({ db, mailTransport, wsPublish })
+if (err) process.exit(-1)
