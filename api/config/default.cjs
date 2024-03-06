1e219c299e0144a7b136373f51d0411c2096f141
BatLeDev <baptiste.guerny@gmail.com>
2024-03-06 14:20:02 +0100

    fix: ui Dockerfile

--- api/config/default.cjs
+++ api/config/default.cjs
@@ -7,9 +7,9 @@ module.exports = {
   },
   defaultTimeZone: 'Europe/Paris',
   directoryUrl: 'http://localhost:5600/simple-directory',
-  mongoUrl: 'mongodb://localhost:27017/data-fair-processings',
+  mongoUrl: 'mongodb://mongo:27017/data-fair-processings',
   origin: 'http://localhost:5600',
-  port: 8082,
+  port: 8080,
   prometheus: {
     active: true
   }
--- api/config/development.cjs
+++ api/config/development.cjs
@@ -1,6 +1,7 @@
 module.exports = {
   dataDir: '../data/development',
   mongoUrl: 'mongodb://localhost:27017/data-fair-processings-development',
+  port: 8082,
   prometheus: {
     active: false
   }
