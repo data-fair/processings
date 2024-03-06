e025f8bbd4d1aa7d996bdf8b3c843e1e1cb876a9
BatLeDev <baptiste.guerny@gmail.com>
2024-03-06 15:05:58 +0100

    fix: mongo url in prod
    - When testing the API Docker image, connection to the database was not working.

--- api/config/default.cjs
+++ api/config/default.cjs
@@ -7,7 +7,7 @@ module.exports = {
   },
   defaultTimeZone: 'Europe/Paris',
   directoryUrl: 'http://localhost:5600/simple-directory',
-  mongoUrl: 'mongodb://mongo:27017/data-fair-processings',
+  mongoUrl: 'mongodb://localhost:27017/data-fair-processings',
   origin: 'http://localhost:5600',
   port: 8080,
   prometheus: {
