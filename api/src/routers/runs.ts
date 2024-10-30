378ae7c104e22b28a21025f68c77ede23a559ef5
BatLeDev <baptiste.guerny@gmail.com>
2024-10-30 10:36:19 +0100

    refactor: add run type

--- api/src/routers/runs.ts
+++ api/src/routers/runs.ts
@@ -1,6 +1,9 @@
-import { session, asyncHandler } from '@data-fair/lib/express/index.js'
+import type { Run } from '#types/index.js'
+import type { SessionStateAuthenticated } from '@data-fair/lib-express/index.js'
+
+import { session, asyncHandler } from '@data-fair/lib-express/index.js'
 import { Router } from 'express'
-import mongo from '@data-fair/lib/node/mongo.js'
+import mongo from '#mongo'
 import findUtils from '../utils/find.js'
 import permissions from '../utils/permissions.js'
 
@@ -9,16 +12,14 @@ export default router
 
 const sensitiveParts = ['permissions']
 
-/** @typedef {import('../../../shared/types/run/index.js').Run} Run */
-
 /**
  * Remove sensitive parts from a run object (permissions)
- * @param {Run} run the run object to clean
- * @param {import('@data-fair/lib/express/index.js').SessionStateAuthenticated} sessionState
- * @param {string|undefined} host req.headers.host
- * @returns {Run} the cleaned run object
+ * @param run the run object to clean
+ * @param sessionState
+ * @param host req.headers.host
+ * @returns the cleaned run object
  */
-const cleanRun = (run, sessionState, host) => {
+const cleanRun = (run: Run, sessionState: SessionStateAuthenticated, host: string | undefined): Run => {
   run.userProfile = permissions.getUserResourceProfile(run.owner, run.permissions, sessionState, host)
   if (run.userProfile !== 'admin') {
     for (const part of sensitiveParts) delete run[part]
@@ -26,42 +27,36 @@ const cleanRun = (run, sessionState, host) => {
   return run
 }
 
-/**
- * @typedef {object} getParams
- * @property {string} size
- * @property {string} page
- * @property {string} skip
- * @property {string} showAll
- * @property {string} sort
- * @property {string} select
- */
+interface GetParams {
+  size: string
+  page: string
+  skip: string
+  showAll: string
+  sort: string
+  select: string
+}
 
 // Get the list of runs (without logs)
 router.get('', asyncHandler(async (req, res) => {
   const sessionState = await session.reqAuthenticated(req)
-  const params = /** @type {getParams} */(req.query)
+  const params: GetParams = req.query as any
   const sort = findUtils.sort(params.sort)
   const [size, skip] = findUtils.pagination(params.size, params.page, params.skip)
   // implicit showAll on runs if we are looking at a processing in adminMode
   if (sessionState.user.adminMode) params.showAll = 'true'
   const query = findUtils.query(req.query, sessionState, { processing: 'processing._id' }) // Check permissions on processing
   const project = { log: 0 }
-  /** @type {import('mongodb').Collection<Run>} */
-  const runsCollection = mongo.db.collection('runs')
   const [runs, count] = await Promise.all([
-    size > 0 ? runsCollection.find(query).limit(size).skip(skip).sort(sort).project(project).toArray() : Promise.resolve([]),
-    runsCollection.countDocuments(query)
+    size > 0 ? mongo.runs.find(query).limit(size).skip(skip).sort(sort).project(project).toArray() : Promise.resolve([]),
+    mongo.runs.countDocuments(query)
   ])
-  // @ts-ignore -> r is a run
-  res.send({ results: runs.map((r) => cleanRun(r, sessionState)), count })
+  res.send({ results: runs.map((r: Run) => cleanRun(r, sessionState, req.headers.host)), count })
 }))
 
 // Get a run (with logs)
 router.get('/:id', asyncHandler(async (req, res) => {
   const sessionState = await session.reqAuthenticated(req)
-  /** @type {import('mongodb').Collection<Run>} */
-  const runsCollection = mongo.db.collection('runs')
-  const run = await runsCollection.findOne({ _id: req.params.id })
+  const run = await mongo.runs.findOne({ _id: req.params.id }) as Run
   if (!run) return res.status(404).send()
   if (!['admin', 'exec', 'read'].includes(permissions.getUserResourceProfile(run.owner, run.permissions, sessionState, req.headers.host) ?? '')) return res.status(403).send()
   if (!sessionState.user.adminMode && run.log) run.log = run.log.filter(l => l.type !== 'debug')
@@ -71,12 +66,10 @@ router.get('/:id', asyncHandler(async (req, res) => {
 // Kill a run
 router.post('/:id/_kill', asyncHandler(async (req, res) => {
   const sessionState = await session.reqAuthenticated(req)
-  /** @type {import('mongodb').Collection<Run>} */
-  const runsCollection = mongo.db.collection('runs')
-  const run = await runsCollection.findOne({ _id: req.params.id })
+  const run = await mongo.runs.findOne({ _id: req.params.id })
   if (!run) return res.status(404).send()
   if (!['admin', 'exec'].includes(permissions.getUserResourceProfile(run.owner, run.permissions, sessionState, req.headers.host) ?? '')) return res.status(403).send()
-  await runsCollection.updateOne({ _id: run._id }, { $set: { status: 'kill' } })
+  await mongo.runs.updateOne({ _id: run._id }, { $set: { status: 'kill' } })
   run.status = 'kill'
   res.send(cleanRun(run, sessionState, req.headers.host))
 }))
