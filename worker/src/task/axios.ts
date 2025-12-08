ae0a074aaf9dfcb516c6718f5ddfea3dca3529b4
Alban Mouton <alban.mouton@gmail.com>
2025-12-08 14:32:46 +0100

    refactor: extract axios related functions

---
+++ worker/src/task/axios.ts
@@ -0,0 +1,110 @@
+import { httpAgent, httpsAgent } from '@data-fair/lib-node/http-agents.js'
+import axios from 'axios'
+import axiosRetry from 'axios-retry'
+import config from '#config'
+import type { Processing } from '#api/types'
+import type { LogFunctions } from '@data-fair/lib-common-types/processings.js'
+
+/**
+ * Create an Axios instance.
+ */
+export const getAxiosInstance = (processing: Processing, log: LogFunctions) => {
+  const headers: Record<string, string> = {
+    'x-apiKey': config.dataFairAPIKey
+  }
+  if (config.dataFairAdminMode) {
+    const account = { ...processing.owner }
+    if (account.name) account.name = encodeURIComponent(account.name)
+    if (account.departmentName) account.departmentName = encodeURIComponent(account.departmentName)
+    headers['x-account'] = JSON.stringify(account)
+  }
+  headers['x-processing'] = JSON.stringify({ _id: processing._id, title: encodeURIComponent(processing.title) })
+
+  const axiosInstance = axios.create({
+    // this is necessary to prevent excessive memory usage during large file uploads, see https://github.com/axios/axios/issues/1045
+    maxRedirects: 0,
+    httpAgent,
+    httpsAgent
+  })
+
+  // apply default base url and send api key when relevant
+  axiosInstance.interceptors.request.use(cfg => {
+    if (!cfg.url) throw new Error('missing url in axios request')
+    if (!/^https?:\/\//i.test(cfg.url)) {
+      if (cfg.url.startsWith('/')) cfg.url = config.dataFairUrl + cfg.url
+      else cfg.url = config.dataFairUrl + '/' + cfg.url
+    }
+    const isDataFairUrl = cfg.url.startsWith(config.dataFairUrl)
+    if (isDataFairUrl) Object.assign(cfg.headers, headers)
+    cfg.headers['User-Agent'] = cfg.headers['User-Agent'] ?? `@data-fair/processings (${processing.plugin})`
+
+    // use private data fair url if specified to prevent leaving internal infrastructure
+    // except from GET requests so that they still appear in metrics
+    // except if config.getFromPrivateDataFairUrl is set to true, then all requests are sent to the private url
+    const usePrivate =
+      config.privateDataFairUrl &&
+      isDataFairUrl &&
+      (config.getFromPrivateDataFairUrl || ['post', 'put', 'delete', 'patch'].includes(cfg.method || ''))
+    if (usePrivate) {
+      cfg.url = cfg.url.replace(config.dataFairUrl, config.privateDataFairUrl!)
+      cfg.headers.host = new URL(config.dataFairUrl).host
+    }
+    return cfg
+  }, error => Promise.reject(error))
+
+  axiosRetry(axiosInstance, {
+    retries: 3,
+    retryDelay: axiosRetry.exponentialDelay,
+    shouldResetTimeout: true,
+    onRetry: (retryCount, _err, requestConfig) => {
+      const err = prepareAxiosError(_err)
+      const message = getHttpErrorMessage(err) || err.message || err
+      log.warning(`tentative ${retryCount} de requête ${requestConfig.method} ${requestConfig.url} : ${message}`)
+    }
+  })
+
+  return axiosInstance
+}
+
+// customize axios errors for shorter stack traces when a request fails
+// WARNING: we used to do it in an interceptor, but it was incompatible with axios-retry
+export const prepareAxiosError = (error: any) => {
+  const response = error.response ?? error.request?.res ?? error.res
+  if (!response) return error
+  delete response.request
+  const headers: Record<string, string> = {}
+  if (response.headers?.location) headers.location = response.headers.location
+  response.headers = headers
+  response.config = response.config ?? error.config
+  if (response.config) {
+    response.config = { method: response.config.method, url: response.config.url, params: response.config.params, data: response.config.data }
+    if (response.config.data && response.config.data._writableState) delete response.config.data
+  }
+  if (response.data && response.data._readableState) delete response.data
+  if (error.message) response.message = error.message
+  if (error.stack) response.stack = error.stack
+  return response
+}
+
+export const getHttpErrorMessage = (err: any) => {
+  let httpMessage = err.status ?? err.statusCode
+  if (httpMessage) {
+    const statusText = err.statusText ?? err.statusMessage
+    if (statusText) httpMessage += ' - ' + statusText
+    if (err.data) {
+      if (typeof err.data === 'string') httpMessage += ' - ' + err.data
+      else httpMessage += ' - ' + JSON.stringify(err.data)
+    } else if (err.message) {
+      httpMessage += ' - ' + err.message
+    }
+    if (err.config && err.config.url) {
+      let url = err.config.url
+      url = url.replace(config.dataFairUrl, '')
+      if (config.privateDataFairUrl) {
+        url = url.replace(config.privateDataFairUrl, '')
+      }
+      httpMessage += ` (${url})`
+    }
+    return httpMessage
+  }
+}
--- worker/src/task/task.ts
+++ worker/src/task/task.ts
@@ -2,20 +2,18 @@ import type { LogFunctions, ProcessingContext } from '@data-fair/lib-common-type
 import type { Account } from '@data-fair/lib-express/index.js'
 import type { Processing, Run } from '#api/types'
 
-import axios from 'axios'
-import axiosRetry from 'axios-retry'
 import util from 'node:util'
 import fs from 'fs-extra'
 import path from 'path'
 import resolvePath from 'resolve-path'
 import tmp from 'tmp-promise'
 import { DataFairWsClient } from '@data-fair/lib-node/ws-client.js'
-import { httpAgent, httpsAgent } from '@data-fair/lib-node/http-agents.js'
 import * as wsEmitter from '@data-fair/lib-node/ws-emitter.js'
 import { decipher } from '@data-fair/processings-shared/cipher.ts'
 import { running } from '../utils/runs.ts'
 import config from '#config'
 import mongo from '#mongo'
+import { getAxiosInstance, getHttpErrorMessage, prepareAxiosError } from './axios.ts'
 
 fs.ensureDirSync(config.dataDir)
 const baseTmpDir = config.tmpDir || path.join(config.dataDir, 'tmp')
@@ -26,76 +24,6 @@ tmp.setGracefulCleanup()
 let pluginModule: { run: (context: ProcessingContext) => Promise<void>, stop?: () => Promise<void> }
 let _stopped: boolean
 const processingsDir = path.join(config.dataDir, 'processings')
-
-/**
- * Create an Axios instance.
- */
-const getAxiosInstance = (processing: Processing) => {
-  const headers: Record<string, string> = {
-    'x-apiKey': config.dataFairAPIKey
-  }
-  if (config.dataFairAdminMode) {
-    const account = { ...processing.owner }
-    if (account.name) account.name = encodeURIComponent(account.name)
-    if (account.departmentName) account.departmentName = encodeURIComponent(account.departmentName)
-    headers['x-account'] = JSON.stringify(account)
-  }
-  headers['x-processing'] = JSON.stringify({ _id: processing._id, title: encodeURIComponent(processing.title) })
-
-  const axiosInstance = axios.create({
-    // this is necessary to prevent excessive memory usage during large file uploads, see https://github.com/axios/axios/issues/1045
-    maxRedirects: 0,
-    httpAgent,
-    httpsAgent
-  })
-
-  // apply default base url and send api key when relevant
-  axiosInstance.interceptors.request.use(cfg => {
-    if (!cfg.url) throw new Error('missing url in axios request')
-    if (!/^https?:\/\//i.test(cfg.url)) {
-      if (cfg.url.startsWith('/')) cfg.url = config.dataFairUrl + cfg.url
-      else cfg.url = config.dataFairUrl + '/' + cfg.url
-    }
-    const isDataFairUrl = cfg.url.startsWith(config.dataFairUrl)
-    if (isDataFairUrl) Object.assign(cfg.headers, headers)
-    cfg.headers['User-Agent'] = cfg.headers['User-Agent'] ?? `@data-fair/processings (${processing.plugin})`
-
-    // use private data fair url if specified to prevent leaving internal infrastructure
-    // except from GET requests so that they still appear in metrics
-    // except if config.getFromPrivateDataFairUrl is set to true, then all requests are sent to the private url
-    const usePrivate =
-      config.privateDataFairUrl &&
-      isDataFairUrl &&
-      (config.getFromPrivateDataFairUrl || ['post', 'put', 'delete', 'patch'].includes(cfg.method || ''))
-    if (usePrivate) {
-      cfg.url = cfg.url.replace(config.dataFairUrl, config.privateDataFairUrl!)
-      cfg.headers.host = new URL(config.dataFairUrl).host
-    }
-    return cfg
-  }, error => Promise.reject(error))
-
-  return axiosInstance
-}
-
-// customize axios errors for shorter stack traces when a request fails
-// WARNING: we used to do it in an interceptor, but it was incompatible with axios-retry
-const prepareAxiosError = (error: any) => {
-  const response = error.response ?? error.request?.res ?? error.res
-  if (!response) return error
-  delete response.request
-  const headers: Record<string, string> = {}
-  if (response.headers?.location) headers.location = response.headers.location
-  response.headers = headers
-  response.config = response.config ?? error.config
-  if (response.config) {
-    response.config = { method: response.config.method, url: response.config.url, params: response.config.params, data: response.config.data }
-    if (response.config.data && response.config.data._writableState) delete response.config.data
-  }
-  if (response.data && response.data._readableState) delete response.data
-  if (error.message) response.message = error.message
-  if (error.stack) response.stack = error.stack
-  return response
-}
 
 /**
  * Create a WebSocket instance.
@@ -171,17 +99,7 @@ export const run = async (mailTransport: any) => {
   const tmpDir = await tmp.dir({ unsafeCleanup: true, tmpdir: baseTmpDir, prefix: `processing-run-${processing._id}-${run._id}` })
   const processingConfig = processing.config || {}
 
-  const axiosInstance = getAxiosInstance(processing)
-  axiosRetry(axiosInstance, {
-    retries: 3,
-    retryDelay: axiosRetry.exponentialDelay,
-    shouldResetTimeout: true,
-    onRetry: (retryCount, _err, requestConfig) => {
-      const err = prepareAxiosError(_err)
-      const message = getHttpErrorMessage(err) || err.message || err
-      log.warning(`tentative ${retryCount} de requête ${requestConfig.method} ${requestConfig.url} : ${message}`)
-    }
-  })
+  const axiosInstance = getAxiosInstance(processing, log)
 
   const secrets: Record<string, string> = {}
   if (processing.secrets) {
@@ -252,29 +170,6 @@ export const stop = async () => {
   if (pluginModule && pluginModule.stop) await pluginModule.stop()
   // grace period of 20s, either run() finishes in the interval or we shutdown
   await new Promise(resolve => setTimeout(resolve, config.worker.gracePeriod))
-}
-
-const getHttpErrorMessage = (err: any) => {
-  let httpMessage = err.status ?? err.statusCode
-  if (httpMessage) {
-    const statusText = err.statusText ?? err.statusMessage
-    if (statusText) httpMessage += ' - ' + statusText
-    if (err.data) {
-      if (typeof err.data === 'string') httpMessage += ' - ' + err.data
-      else httpMessage += ' - ' + JSON.stringify(err.data)
-    } else if (err.message) {
-      httpMessage += ' - ' + err.message
-    }
-    if (err.config && err.config.url) {
-      let url = err.config.url
-      url = url.replace(config.dataFairUrl, '')
-      if (config.privateDataFairUrl) {
-        url = url.replace(config.privateDataFairUrl, '')
-      }
-      httpMessage += ` (${url})`
-    }
-    return httpMessage
-  }
 }
 
 export default { run, stop }
