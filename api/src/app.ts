0c39b801fe7c53b80ec1a55dcb963c7e9db9603b
BatLeDev <baptiste.guerny@koumoul.com>
2026-03-30 11:22:05 +0200

    fix: add connect-src CSP directive for vjsf remote fetches

--- api/src/app.ts
+++ api/src/app.ts
@@ -35,6 +35,8 @@ if (process.env.NODE_ENV !== 'test') {
   const cspDirectives = { ...defaultNonceCSPDirectives }
   // necessary to use vjsf without pre-compilation
   cspDirectives['script-src'] = "'unsafe-eval' " + defaultNonceCSPDirectives['script-src']
+  // necessary for vjsf to fetch remote services
+  cspDirectives['connect-src'] = "'self' https:"
   app.use(await createSpaMiddleware(resolve(import.meta.dirname, '../../ui/dist'), uiConfig, {
     csp: {
       nonce: true,
