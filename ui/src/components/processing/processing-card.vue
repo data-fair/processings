8a95d176c979c8730b492b410815954d93f05296
BatLeDev <baptiste.guerny@koumoul.com>
2026-09-16 09:36:06 +0200

    refactor(ui): use the lib composable to label a deleted department

--- ui/src/components/processing/processing-card.vue
+++ ui/src/components/processing/processing-card.vue
@@ -18,7 +18,7 @@
       <template #append>
         <owner-avatar
           v-if="showOwner"
-          :owner="displayOwner(processing.owner)"
+          :owner="processing.owner"
         />
       </template>
     </v-card-item>
@@ -177,7 +177,6 @@
 <script setup lang="ts">
 import ownerAvatar from '@data-fair/lib-vuetify/owner-avatar.vue'
 const { t } = useI18n()
-const { displayOwner } = useDisplayOwner()
 const { dayjs } = useLocaleDayjs()
 
 const props = defineProps({
